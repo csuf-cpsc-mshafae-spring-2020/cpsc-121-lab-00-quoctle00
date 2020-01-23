f9432f8ebf1955244fcfeb01bb5b6505efc8504e
Quoc Le <quoctle00@csu.fullerton.edu>
2020-01-23 15:11:21 -0800

    added the loop operators and argc

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -1,11 +1,24 @@
-// Oscar Mayer
-// CPSC 121-99
-// oscar.mayer@csu.fullerton.edu
+// Quoc Le
+// CPSC 121-03
+// quoctle00@csu.fullerton.edu
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[]){
-  cout << "Hello world!" << endl;
+
+  if(argc < 3){
+    cout << "need more info or inputs" << endl;
+  }
+
+  string user_name = argv[1];
+
+  string times_to_print = argv[2];
+  int times_to_print_int = std::stoi(times_to_print);
+
+for(int i = 0; i < times_to_print_int; i++) {
+  cout << "Hello " << user_name << "! " << endl;
+
+}
   return 0;
 }
